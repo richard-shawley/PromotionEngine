3ea29c4b1d3a44f9a57c137558af90a1aef1fb77
richardshawley <richard.shawley@cogcoder.com>
2021-11-28 12:10:31 +0000

    Added test project dependencies Created feature file and step definition skeleton

---
+++ tests/Shopping.Specs/Features/PromotionEngineSpecs.feature
@@ -0,0 +1,37 @@
+﻿Feature: PromotionEngineSpecs
+
+This feature is test the promotion engine behaivour against the scenarios outlined in the brief
+
+Background:
+	Given product 'A' costs '50.0'
+	And product 'B' costs '30.0'
+	And product 'C' costs '20.0'
+	And product 'D' costs '15.0'
+	And there is a promotion for '3' of product 'A' costing '130.0'
+	And there is a promotion for '2' of product 'B' costing '45.0'
+	And there is a promotion for products 'C, D' costing '30.0'
+
+@promotionEngine
+Scenario: A
+	Given the cart contains '1' of product 'A'
+	And the cart contains '1' of product 'B'
+	And the cart contains '1' of product 'C'
+	When the promotion engine is triggered
+	Then the calculated total is '100.0'
+
+@promotionEngine
+Scenario: B
+	Given the cart contains '5' of product 'A'
+	And the cart contains '5' of product 'B'
+	And the cart contains '1' of product 'C'
+	When the promotion engine is triggered
+	Then the calculated total is '370.0'
+
+@promotionEngine
+Scenario: C
+	Given the cart contains '3' of product 'A'
+	And the cart contains '5' of product 'B'
+	And the cart contains '1' of product 'C'
+	And the cart contains '1' of product 'D'
+	When the promotion engine is triggered
+	Then the calculated total is '280.0'
---
+++ tests/Shopping.Specs/Steps/PromotionEngineSpecsStepDefinitions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Shopping.Specs.Context;
+using TechTalk.SpecFlow;
+
+namespace Shopping.Specs
+{
+    [Binding]
+    public class PromotionEngineSpecsStepDefinitions
+    {
+        private readonly PromotionEngineContext _promotionEngineContext;
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public PromotionEngineSpecsStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"product '(.*)' costs '(.*)'")]
+        public void GivenProductCosts(string sku, decimal price)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Given(@"there is a promotion for '(.*)' of product '(.*)' costing '(.*)'")]
+        public void GivenThereIsAPromotionForOfProductCosting(int quantity, string sku, decimal price)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Given(@"there is a promotion for products '(.*)' costing '(.*)'")]
+        public void GivenThereIsAPromotionForProductsCosting(string skuList, decimal price)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Given(@"the cart contains '(.*)' of product '(.*)'")]
+        public void GivenTheCartContainsOfProduct(int quantity, string sku)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [When(@"the promotion engine is triggered")]
+        public void WhenThePromotionEngineIsTriggered()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"the calculated total is '(.*)'")]
+        public void ThenTheCalculatedTotalIs(decimal calculatedTotal)
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
+
