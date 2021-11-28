e6bfa60d9dccf74b960d96cb249fbf1a590ca901
richardshawley <richard.shawley@cogcoder.com>
2021-11-28 13:04:51 +0000

    Added in memory repositories

---
+++ src/Shopping.Infrastructure/Persistence/InMemoryProducts.cs
@@ -0,0 +1,14 @@
+﻿using Shopping.Domain.Contracts;
+using Shopping.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Infrastructure.Persistence
+{
+    public class InMemoryProducts : InMemoryRepository<Product>, IProductRepository
+    {
+    }
+}
---
+++ src/Shopping.Infrastructure/Persistence/InMemoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Infrastructure.Persistence
+{
+    public class InMemoryRepository<T>
+    {
+        private readonly ConcurrentBag<T> _entities = new ConcurrentBag<T>();
+
+        public void Add(T entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            _entities.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+            => _entities;
+
+        public IQueryable<T> Query()
+            => _entities.AsQueryable();
+    }
+}
