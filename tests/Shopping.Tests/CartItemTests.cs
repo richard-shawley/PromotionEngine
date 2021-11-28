cb2ef8437f8e5c301f526cf840703dfe70052bb2
richardshawley <richard.shawley@cogcoder.com>
2021-11-28 12:23:05 +0000

    Added Cart and CartItem tests

---
+++ tests/Shopping.Tests/CartItemTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Shopping.Domain.Entities;
+using Shopping.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Tests
+{
+    [TestFixture]
+    public class CartItemTests
+    {
+        [Test]
+        public void IncreaseQuantity_IncreasesQuantity()
+        {
+            var cartItem = new CartItem(
+                new Product
+                {
+                    Sku = "TEST",
+                    Price = 50.0M
+                },
+                1);
+
+            cartItem.IncreaseQuantity(2);
+
+            cartItem.Quantity.Should().Be(3);
+        }
+
+        [Test]
+        public void DecreaseQuantity_DecreasesQuantity()
+        {
+            var cartItem = new CartItem(
+                new Product
+                {
+                    Sku = "TEST",
+                    Price = 50.0M
+                },
+                3);
+
+            cartItem.DecreaseQuantity(2);
+
+            cartItem.Quantity.Should().Be(1);
+        }
+
+        [Test]
+        public void DecreaseQuantity_ThrowsIfQuantityZero()
+        {
+            var cartItem = new CartItem(
+                new Product
+                {
+                    Sku = "TEST",
+                    Price = 50.0M
+                },
+                3);
+
+            Action act = () => cartItem.DecreaseQuantity(3);
+
+            act.Should().Throw<ArgumentOutOfRangeException>();
+        }
+    }
+}
---
+++ tests/Shopping.Tests/CartTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Shopping.Domain.Entities;
+using Shopping.Domain.Models;
+using System;
+
+namespace Shopping.Tests
+{
+    [TestFixture]
+    public class CartTests
+    {
+        [Test]
+        public void AddItem_AddsItem()
+        {
+            var cart = new Cart();
+            var cartItem = new CartItem(
+                new Product
+                {
+                    Sku = "TEST",
+                    Price = 50.0M
+                },
+                1);
+
+            cart.AddItem(cartItem);
+
+            cart.Items.Should().Contain(cartItem);
+        }
+
+        [Test]
+        public void AddItem_IncrementsExistingItem()
+        {
+            var cart = new Cart();
+            var product = new Product
+            {
+                Sku = "TEST",
+                Price = 50.0M
+            };
+
+            var cartItem = new CartItem(product, 1);
+
+            cart.AddItem(cartItem);
+
+            cart.AddItem(cartItem);
+
+            var expectedItem = new CartItem(product, 2);
+
+            cart.Items.Should().ContainEquivalentOf(expectedItem);
+        }
+
+        [Test]
+        public void RemoveItem_RemovesItem()
+        {
+            var cart = new Cart();
+            var product = new Product
+            {
+                Sku = "TEST",
+                Price = 50.0M
+            };
+
+            var cartItem = new CartItem(product, 5);
+
+            cart.AddItem(cartItem);
+
+            cart.RemoveItem(cartItem);
+
+            cart.Items.Should().BeEmpty();
+        }
+
+        [Test]
+        public void RemoveItem_DecrementsItem()
+        {
+            var cart = new Cart();
+            var product = new Product
+            {
+                Sku = "TEST",
+                Price = 50.0M
+            };
+
+            var cartItem = new CartItem(product, 5);
+
+            cart.AddItem(cartItem);
+
+            var removeItem = new CartItem(product, 2);
+
+            cart.RemoveItem(removeItem);
+
+            var expectedItem = new CartItem(product, 3);
+
+            cart.Items.Should().ContainEquivalentOf(expectedItem);
+        }
+    }
+}
