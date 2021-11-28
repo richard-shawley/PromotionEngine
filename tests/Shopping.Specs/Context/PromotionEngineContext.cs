489165fff6638ab9b16558eb47872578e3e3456b
richardshawley <richard.shawley@cogcoder.com>
2021-11-28 12:46:16 +0000

    Implemented promotion engine test steps

--- tests/Shopping.Specs/Context/PromotionEngineContext.cs
+++ tests/Shopping.Specs/Context/PromotionEngineContext.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using Shopping.Domain.Contracts;
+using Shopping.Domain.Entities;
+using Shopping.Domain.Models;
 
 namespace Shopping.Specs.Context
 {
     public class PromotionEngineContext
     {
+        private readonly Mock<IPromotionService> _promotionService;
+        public PromotionEngineContext()
+        {
+            _promotionService = new Mock<IPromotionService>();
+        }
+
+        private PromotionEngine GetPromotionEngine()
+        {
+            _promotionService.Setup(o => o.GetPromotions()).Returns(() => Promotions);
+            return new PromotionEngine(_promotionService.Object);
+        }
+
+        public List<MultiProductPromotion> Promotions { get; set; } = new List<MultiProductPromotion>();
+        public PromotionEngine PromotionEngine => GetPromotionEngine();
+        public Cart Cart { get; set; } = new Cart();
+        public List<Product> Products { get; set; } = new List<Product>();
+        public Checkout Checkout { get; set; }
     }
 }
--- tests/Shopping.Specs/Steps/PromotionEngineSpecsStepDefinitions.cs
+++ tests/Shopping.Specs/Steps/PromotionEngineSpecsStepDefinitions.cs
@@ -1,6 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using FluentAssertions;
+using Shopping.Domain.Entities;
+using Shopping.Domain.Models;
 using Shopping.Specs.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using TechTalk.SpecFlow;
 
 namespace Shopping.Specs
@@ -10,48 +14,83 @@ namespace Shopping.Specs
     {
         private readonly PromotionEngineContext _promotionEngineContext;
 
-        private readonly ScenarioContext _scenarioContext;
-
-        public PromotionEngineSpecsStepDefinitions(ScenarioContext scenarioContext)
+        public PromotionEngineSpecsStepDefinitions(PromotionEngineContext promotionEngineContext)
         {
-            _scenarioContext = scenarioContext;
+            _promotionEngineContext = promotionEngineContext;
         }
 
         [Given(@"product '(.*)' costs '(.*)'")]
         public void GivenProductCosts(string sku, decimal price)
         {
-            _scenarioContext.Pending();
+            _promotionEngineContext.Products.Add(
+                new Product
+                {
+                    Sku = sku,
+                    Price = price
+                });
         }
 
         [Given(@"there is a promotion for '(.*)' of product '(.*)' costing '(.*)'")]
         public void GivenThereIsAPromotionForOfProductCosting(int quantity, string sku, decimal price)
         {
-            _scenarioContext.Pending();
+
+            var promotionCriteria = CreatePromotionCriteria(quantity, sku);
+
+            _promotionEngineContext.Promotions.Add(new MultiProductPromotion
+            {
+                PromotionCriteria = new List<PromotionCriteria> {
+                    promotionCriteria
+                },
+                Price = price
+            });
         }
 
         [Given(@"there is a promotion for products '(.*)' costing '(.*)'")]
         public void GivenThereIsAPromotionForProductsCosting(string skuList, decimal price)
         {
-            _scenarioContext.Pending();
+            var skus = Array.ConvertAll(skuList.Split(','), p => p.Trim());
+            var promotionCriteria = new List<PromotionCriteria>();
+
+            foreach (var sku in skus)
+            {
+                promotionCriteria.Add(CreatePromotionCriteria(1, sku));
+            }
+
+            _promotionEngineContext.Promotions.Add(new MultiProductPromotion
+            {
+                PromotionCriteria = promotionCriteria,
+                Price = price
+            });
         }
 
         [Given(@"the cart contains '(.*)' of product '(.*)'")]
         public void GivenTheCartContainsOfProduct(int quantity, string sku)
         {
-            _scenarioContext.Pending();
+            _promotionEngineContext.Cart.AddItem(new CartItem(GetProduct(sku), quantity));
         }
 
         [When(@"the promotion engine is triggered")]
         public void WhenThePromotionEngineIsTriggered()
         {
-            _scenarioContext.Pending();
+            _promotionEngineContext.Checkout = new Checkout(_promotionEngineContext.PromotionEngine);
         }
 
         [Then(@"the calculated total is '(.*)'")]
         public void ThenTheCalculatedTotalIs(decimal calculatedTotal)
         {
-            _scenarioContext.Pending();
+            _promotionEngineContext.Checkout.TotalAfterDiscount(_promotionEngineContext.Cart).Should().Be(calculatedTotal);
         }
+
+        private PromotionCriteria CreatePromotionCriteria(int quantity, string sku)
+        {
+            return new PromotionCriteria
+            {
+                Product = GetProduct(sku),
+                Quantity = quantity
+            };
+        }
+
+        private Product GetProduct(string sku)
+            => _promotionEngineContext.Products.Single(p => p.Sku == sku);
     }
 }
-
