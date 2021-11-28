72b121fda549578f02d8ddaaa69712126dfe29b1
richardshawley <richard.shawley@cogcoder.com>
2021-11-28 12:43:06 +0000

    Added Checkout model

---
+++ src/Shopping.Domain/Models/Checkout.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Domain.Models
+{
+    public class Checkout
+    {
+        private PromotionEngine _promotionEngine;
+        public Checkout(PromotionEngine promotionEngine)
+        {
+            _promotionEngine = promotionEngine ?? throw new ArgumentNullException(nameof(promotionEngine));
+        }
+
+        public decimal Discount(Cart cart)
+        {
+            if (cart == null) throw new ArgumentNullException(nameof(cart));
+
+            return _promotionEngine.GetDiscount(cart.Items);
+        }
+
+        public decimal TotalAfterDiscount(Cart cart)
+        {
+            if (cart == null) throw new ArgumentNullException(nameof(cart));
+
+            return cart.Total - Discount(cart);
+        }
+    }
+}
