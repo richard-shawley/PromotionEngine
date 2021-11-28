f10d37b21aa3590bd24f3d90d80a9f329cf0cbb1
richardshawley <richard.shawley@cogcoder.com>
2021-11-28 13:09:41 +0000

    Added ServiceCollection extension methods

---
+++ src/Shopping.Domain/DependencyInjection/ShoppingServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shopping.Domain.Contracts;
+using Shopping.Domain.Models;
+using Shopping.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Domain.DependencyInjection
+{
+    public static class ShoppingServiceCollectionExtensions
+    {
+        public static ServiceCollection AddShopping(this ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IProductService, ProductService>();
+            serviceCollection.AddTransient<IPromotionService, PromotionService>();
+            serviceCollection.AddTransient<PromotionEngine>();
+            serviceCollection.AddTransient<Cart>();
+            serviceCollection.AddTransient<Checkout>();
+            return serviceCollection;
+        }
+    }
+}
---
+++ src/Shopping.Infrastructure/DependencyInjection/InMemoryRepositoryServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shopping.Domain.Contracts;
+using Shopping.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Infrastructure.DependencyInjection
+{
+    public static class InMemoryRepositoryServiceCollectionExtension
+    {
+        public static ServiceCollection AddInMemoryRepositories(this ServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IProductRepository, InMemoryProducts>();
+            serviceCollection.AddSingleton<IPromotionRepository, InMemoryPromotions>();
+            return serviceCollection;
+        }
+    }
+}
